b4647387ed95bbb9748d3bd0b26c994d6f2118d9
Zahar <Zahar>
2021-04-09 02:40:09 +0300

    useEffect hook (85)

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHook.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHook.jsx
@@ -1,8 +1,12 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const ProfileStatusWithHook = (props) => {
     let [status,setStatus] = useState(props.status);
     let [editMode,setEditMode] = useState(false);
+
+    useEffect(() => {
+        setStatus(props.status)
+    },[props.status])
 
     const statusOnChange = (e) => {
         setStatus(e.currentTarget.value);
